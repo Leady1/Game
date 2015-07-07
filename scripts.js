273fc19ad9b7471f84f5cd4589d74abbe87f2754
Matthew Leadbetter <dj.leady@hotmail.co.uk>
2015-07-07 15:24:13 +0100

    Update

--- scripts.js
+++ scripts.js
@@ -2,10 +2,18 @@ var game = new Phaser.Game(800,600,Phaser.AUTO,"platformer",{preload: preload, c
 var character;
 function preload()
 {
+game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
 game.load.spritesheet("Character", "Images/sprites_6.png",16,16,64);
+game.load.image("Level Tiles", "images/sprites_6.png");
 }
 function create()
 {
+    var map = game.add.tilemap("Level", 16, 16);
+map.addTilesetImage("Level Tiles");
+
+var layer = map.createLayer(0);
+layer.resizeWorld();
+
 character= game.add.sprite(100, 100, "Character");
 }
 function update()
@@ -14,5 +22,5 @@ function update()
     character.x = character.x + 5;
 
     if (game.input.keyboard.isDown(Phaser.Keyboard.A))
-    character.x = character.x - 1;
+    character.x = character.x - 5;
 }
