61f679e808d6277db331a5de2e94266de74b16e5
Matthew Leadbetter <dj.leady@hotmail.co.uk>
2015-07-07 16:39:46 +0100

    New

--- scripts.js
+++ scripts.js
@@ -1,26 +1,28 @@
-var game = new Phaser.Game(800,600,Phaser.AUTO,"platformer",{preload: preload, create: create,update: update});
+var game = new Phaser.Game(912,600,Phaser.AUTO,"platformer",{preload: preload, create: create,update: update});
 var character;
+ 
 function preload()
 {
-game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
-game.load.spritesheet("Character", "Images/sprites_6.png",16,16,64);
-game.load.image("Level Tiles", "images/sprites_6.png");
+    game.stage.backgroundColor = "#451100";
+    game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
+    game.load.spritesheet("Character", "Images/sprites_6.png",16,16,64);
+    game.load.image("Level Tiles", "images/sprites_6.png");
 }
+ 
 function create()
 {
     var map = game.add.tilemap("Level", 16, 16);
-map.addTilesetImage("Level Tiles");
-
+    map.addTilesetImage("Level Tiles");
 var layer = map.createLayer(0);
-layer.resizeWorld();
-
-character= game.add.sprite(100, 100, "Character");
-}
+    layer.resizeWorld();
+    character= game.add.sprite(1264, 944, "Character");
+     game.camera.follow(character);
+} 
 function update()
 {
     if (game.input.keyboard.isDown(Phaser.Keyboard.D))
-    character.x = character.x + 5;
-
+        character.x = character.x + 5;
+ 
     if (game.input.keyboard.isDown(Phaser.Keyboard.A))
-    character.x = character.x - 5;
+        character.x = character.x - 5;
 }
