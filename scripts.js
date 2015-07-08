9dbc6ff8f4bb7d8afe7dc74551449ea1083d676a
Matthew Leadbetter <dj.leady@hotmail.co.uk>
2015-07-08 14:18:26 +0100

    Update

--- scripts.js
+++ scripts.js
@@ -1,14 +1,13 @@
 var game = new Phaser.Game(912,600,Phaser.AUTO,"platformer",{preload: preload, create: create,update: update});
 var character;
 var layer;
-game.physics.arcade.collide(character, layer);
  
 function preload()
 {
     game.stage.backgroundColor = "#451100";
     game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet("Character", "Images/sprites_6.png",16,16,64);
-    game.load.image("Level Tiles", "images/sprites_6.png");
+    game.load.image("Level Tiles", "Images/sprites_6.png");
    
 }
  
@@ -17,16 +16,18 @@ function create()
     game.physics.startSystem(Phaser.Physics.ARCADE)
     var map = game.add.tilemap("Level", 16, 16);
     map.addTilesetImage("Level Tiles");
-layer = map.createLayer(0);
+    layer = map.createLayer(0);
     layer.resizeWorld();
     map.setCollisionBetween(32, 56);
-    character= game.add.sprite(1264, 944, "Character");
-     game.camera.follow(character);
-      character.animations.add("walk",[3,4,5])
-     character.animations.add("idle",[0])
-     character.animations.add("jump",[5])
-      game.physics.arcade.enable(character);
-      character.body.gravity.y = 10;
+    character= game.add.sprite(1272, 944, "Character");
+    game.camera.follow(character);
+    character.animations.add("walk",[3,4,5])
+    character.animations.add("idle",[0])
+    character.animations.add("jump",[5])
+    character.anchor.x = 0.5;
+    character.anchor.y = 1;
+    game.physics.arcade.enable(character);
+    character.body.gravity.y = 300;
        
     
 } 
@@ -36,28 +37,35 @@ function update()
     var isKeyPressed = false
     if (game.input.keyboard.isDown(Phaser.Keyboard.D))
 {
-    character.body.velocity.x = 1;
+    character.body.velocity.x = 100;
     isKeyPressed = true
     character.scale.set(1,1);
     character.animations.play('walk', 10, true);
 } 
     if (game.input.keyboard.isDown(Phaser.Keyboard.A))
 {        
-   character.body.velocity.x = 1;
+   character.body.velocity.x = -100;
     character.animations.play('walk', 10, true);
     character.scale.set(-1, 1);
     isKeyPressed = true
 }
     if (game.input.keyboard.isDown(Phaser.Keyboard.W))
 {
-    character.y = character.y - 3.5 ;
+    if (character.body.onFloor())
+    {
+        character.body.velocity.y = -200
+    }
+   {
     isKeyPressed = true
-     character.animations.play("jump",20,true)
+    character.animations.play("jump",20,true)
+   }
 }
     if (isKeyPressed == false)
 {
+    character.body.velocity.x = 0
     character.animations.play('idle', 20, true);
 }
-
-character.body.gravity.y = 300;
 }
+
+
+
