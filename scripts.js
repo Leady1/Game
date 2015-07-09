1430f8e0e036a99d2236a4d3a71faccb051c5df4
Matthew Leadbetter <dj.leady@hotmail.co.uk>
2015-07-09 14:20:12 +0100

    Update

--- scripts.js
+++ scripts.js
@@ -4,11 +4,13 @@ var layer;
 var lava; 
 var map;
 var map2;
+var mapName;
 function preload()
 {
     game.stage.backgroundColor = "#451100";
     game.load.tilemap("Level", "level.json", null, Phaser.Tilemap.TILED_JSON);
     game.load.tilemap("Level 2", "level 2.json", null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap("Finish", "Finish.json", null, Phaser.Tilemap.TILED_JSON);
     game.load.spritesheet("Character", "Images/sprites_6.png",16,16,64);
     game.load.image("Level Tiles", "Images/sprites_6.png");
 
@@ -29,15 +31,14 @@ function create()
     character.body.gravity.y = 500;
     character.body.setSize (5.9, 16, 1)
     changeLevel("Level");
-    
-    
 } 
+
 function update()
 {
-   var tileX = Math.floor(character.x / 16);
-var tileY = Math.floor(character.y / 16);
-character.body.collideWorldBounds = true;
-var currentTile = map.getTile(tileX, tileY);
+    var tileX = Math.floor(character.x / 16);
+    var tileY = Math.floor(character.y / 16);
+    character.body.collideWorldBounds = true;
+    var currentTile = map.getTile(tileX, tileY);
     game.physics.arcade.collide(character, layer);
     var isKeyPressed = false
     if (game.input.keyboard.isDown(Phaser.Keyboard.D))
@@ -49,7 +50,7 @@ var currentTile = map.getTile(tileX, tileY);
 } 
     if (game.input.keyboard.isDown(Phaser.Keyboard.A))
 {        
-   character.body.velocity.x = -100;
+    character.body.velocity.x = -100;
     character.animations.play('walk',15, true);
     character.scale.set(-1, 1);
     isKeyPressed = true
@@ -57,9 +58,9 @@ var currentTile = map.getTile(tileX, tileY);
     if (game.input.keyboard.isDown(Phaser.Keyboard.W))
 {
     if (character.body.onFloor())
-    {
+   {
         character.body.velocity.y = -200
-    }
+   }
    {
     isKeyPressed = true
     character.animations.play("jump",20,true)
@@ -80,6 +81,13 @@ if (currentTile != null)
     {
         finish();
     }    
+    if (mapName=="Finish")
+    {
+    var style = { font: "65px Arial", fill: "#FFFFFF", align: "center" };
+    var text = game.add.text(game.world.centerX, game.world.centerY, "\nWell Done!\nYou Have Won", style);
+
+    text.anchor.set(0.5);
+    }
 }
 
    
@@ -97,21 +105,39 @@ function onTouchLava()
     
     if (mapName == "Level 2")
     {
-        character.body.velocity.y=0
+     character.body.velocity.y=0
      character.body.velocity.x=0
      character.x = 1064
      character.y = 1080
     }
 }
 function finish()
-{
+    {
+         
+        if (mapName == "Level")
+    {     
      layer.destroy(true);
      character.body.velocity.y=0
      character.body.velocity.x=0
      character.x = 1064
      character.y = 1080
      changeLevel("Level 2");
-}
+     
+         
+     } else {
+        
+     layer.destroy(true);
+     character.body.velocity.y=0
+     character.body.velocity.x=0
+     character.x = game.world.centerX
+     character.y = game.world.centerY
+      
+     changeLevel("Finish");
+      
+     
+     }     
+    
+    }
 function changeLevel(newLevelName)
 {
     if (map != null)
